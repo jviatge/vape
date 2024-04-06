0009b1f3a7aa148d7e827b1ac0d8440042a8e2c5
jviatge <julien.viatge@gmail.com>
2024-04-06 16:21:35 +0200

    WIP

--- tools.ts
+++ tools.ts
@@ -1,15 +1,15 @@
 import { useMutation, useQueries } from "@tanstack/react-query";
 import { queryDeleteByModule, queryDeleteMulitpleByModule } from "./actions/queries";
 import { Loading } from "./components/ui/loading";
-import { queryClient } from "./lib/queryClient";
 import { logger } from "./lib/logs";
+import { queryClient } from "./lib/queryClient";
 
 export {
     Loading,
+    logger,
     queryClient,
     queryDeleteByModule,
     queryDeleteMulitpleByModule,
     useMutation,
     useQueries,
-    logger,
 };
