182f8b2fbbc82ed6671f282cc0bce32d41f7b83e
Julien Viatgé <julien.viatge@gmail.com>
2024-05-15 21:21:43 +0200

    FEAT: logger

--- lib/logs.ts
+++ lib/logs.ts
@@ -1,3 +1,7 @@
+"use server";
+
+import fs from "fs";
+import path from "path";
 import { color } from "./color";
 import { formatDate } from "./formatDate";
 
@@ -6,68 +10,70 @@ const dateNow = () => `[${formatDate(new Date())}]`;
 export const logSuccess = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.green(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logInfo = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.cyan(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logError = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.red(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logWarning = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.yellow(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logQuery = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.magenta(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logServerAction = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.cyan(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logRsc = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.green(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
 export const logApi = (message: string) => {
     const logMessage = `${color.blue(dateNow())} ${color.bgCyan(message)}`;
     console.log(logMessage);
+    saveLogFileDaily(message);
 };
 
-export class logger {
-    private static instance: logger;
+const saveLogFileDaily = (message: string) => {
+    const logsPath = path.join(process.cwd(), "../logs");
 
-    private constructor() {}
+    const formatDateOk = (date: Date) => {
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Les mois commencent à 0, donc on ajoute 1
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+    };
 
-    static getInstance() {
-        if (!logger.instance) {
-            logger.instance = new logger();
+    if (!fs.existsSync(logsPath)) {
+        fs.mkdirSync(logsPath);
+    }
+
+    const nameFile = `../logs/log-${formatDateOk(new Date())}.txt`;
+
+    fs.appendFile(nameFile, `[${new Date().toISOString()}] : ${message}\n`, (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
         }
-        return logger.instance;
-    }
-
-    success(message: string) {
-        logSuccess(message);
-    }
-
-    info(message: string) {
-        logInfo(message);
-    }
-
-    error(message: string) {
-        logError(message);
-    }
-
-    warning(message: string) {
-        logWarning(message);
-    }
-}
+    });
+};
--- tools.ts
+++ tools.ts
@@ -1,12 +1,10 @@
 import { useMutation, useQueries } from "@tanstack/react-query";
 import { queryDeleteByModule, queryDeleteMulitpleByModule } from "./actions/queries";
 import { Loading } from "./components/ui/loading";
-import { logger } from "./lib/logs";
 import { queryClient } from "./lib/queryClient";
 
 export {
     Loading,
-    logger,
     queryClient,
     queryDeleteByModule,
     queryDeleteMulitpleByModule,
