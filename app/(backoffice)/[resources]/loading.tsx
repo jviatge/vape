705bfb9a13ca82d48936551c41a06b30a9f55454
Julien Viatgé <julien.viatge@gmail.com>
2024-05-22 12:42:49 +0200

    FEAT: loading manage

--- app/(backoffice)/[resources]/loading.tsx
+++ app/(backoffice)/[resources]/loading.tsx
@@ -2,7 +2,7 @@ import { Loading } from "@vape/components/ui/loading";
 
 export default function loading() {
     return (
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-10 dark:bg-opacity-20 z-50">
+        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-10 dark:bg-opacity-20 z-50">
             <Loading />
         </div>
     );
