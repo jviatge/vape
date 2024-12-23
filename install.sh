579d205d86e3f0436ec0ac8a181ec42d0c821156
jviatge <julien.viatge@gmail.com>
2024-12-23 12:04:01 +0100

    FEAT: install init project

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-#git clone https://github.com/jviatge/vape.git .vape
 PROJECT_NAME=${1:-"project-vape"}
+git clone https://github.com/jviatge/vape.git .vape
 node ./.vape/install/initFomSH.js $PROJECT_NAME
 cd $PROJECT_NAME
 pnpm install
