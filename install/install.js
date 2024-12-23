da7434a29d97e081bb3bf71555b3e6e75832d678
jviatge <julien.viatge@gmail.com>
2024-12-23 01:14:09 +0100

    FEAT: install init project

--- install/install.js
+++ install/install.js
@@ -3,14 +3,23 @@ const fs = require("fs");
 async function main() {
     console.log("+ Install Vape +");
     const nameProject = "my-project";
+    const pathToProject = "./../" + nameProject;
 
     // mkdir nameProject
     console.log("=> Create project folder");
-    await fs.mkdirSync("./../" + nameProject);
+    await fs.mkdirSync(pathToProject);
 
     // move all in app to nameProject
     console.log("=> Move all in app to project folder");
-    await fs.renameSync("./install/app", "./../" + nameProject);
+    await fs.renameSync("./install/app", pathToProject);
+
+    // cp .env.example to nameProject
+    console.log("=> Create .env file (app)");
+    await fs.copyFileSync("./install/app/.env.example", pathToProject + "/.env");
+
+    // cp .env.example to nameProject
+    console.log("=> Create .env file (db)");
+    await fs.copyFileSync("./install/app/database/.env.example", pathToProject + "/database/.env");
 
     // rename vape to .vape
     console.log("=> Rename .vape folder");
@@ -18,7 +27,7 @@ async function main() {
 
     // move .vape to nameProject
     console.log("=> Move .vape to project folder");
-    await fs.renameSync("./../.vape", "./../" + nameProject + "/.vape");
+    await fs.renameSync("./../.vape", pathToProject + "/.vape");
 }
 
 main();
